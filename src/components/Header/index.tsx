92a824c54686febc96b436b76020c6f64ee4260b
antoineskt <billet.antoine42@gmail.com>
2023-08-02 11:38:12 +0200

    add navbar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,32 @@
 // eslint-disable-next-line import/no-named-as-default
 import styled from 'styled-components'
 import { ReactComponent as Logoab } from '../../assets/logoab.svg'
+import { useState } from 'react'
 
 export default function Header() {
-  const Nav = styled.nav`
+  const [visible, setVisible] = useState(false)
+
+  const HeaderDiv = styled.header`
     display: flex;
-    align-items: center;
+    height: 60px;
+    width: 100%;
     justify-content: space-between;
+    align-items: center;
+    background-color: white;
+  `
+
+  const Nav = styled.nav`
+    visibility: ${visible === true ? 'visible' : 'hidden'};
+    position: fixed;
+    width: 300px;
+    background: red;
+    right: 0;
+    top: 0;
+    margin: 0;
+    height: 100%;
+    background-color: white;
+    transform: ${visible === true ? 'translateX(0%);' : 'translateX(105%);'};
+    transition: transform 1s;
   `
 
   const DivLogo = styled.div`
@@ -16,17 +36,37 @@ export default function Header() {
     }
   `
 
+  const DivUl = styled.div`
+    justify-content: space-between;
+    height: calc(100% - 64px);
+    padding: 32px 0;
+  `
+
+  const Ul = styled.ul`
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+  `
+
   return (
-    <Nav>
+    <HeaderDiv>
       <DivLogo>
         <Logoab />
       </DivLogo>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Projects</li>
-        <li>Contact</li>
-      </ul>
-    </Nav>
+      <button onClick={() => setVisible(true)}>=</button>
+      <Nav>
+        <div>
+          <button onClick={() => setVisible(false)}>X</button>
+        </div>
+        <DivUl>
+          <Ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+          </Ul>
+        </DivUl>
+      </Nav>
+    </HeaderDiv>
   )
 }
